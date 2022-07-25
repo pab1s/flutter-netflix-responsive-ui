a0c9c66c31c1e1d38dc9488839e980e12e586f80
Pablo Olivares <pablolivares1502@gmail.com>
2022-07-26 00:12:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Flutter Netflix Responsive UI Tutorial | Web and Mobile
+
+A Netflix User Interface Clone project to learn Flutter!
+
+
+
+![](screenshots/web.png)
+
+
+
+![](screenshots/mobile0.png)
+
+## Information
+
+This is a Netflix UI clone for mobile and web done to practice some Flutter and Dart! To execute this clone, just run it as a you would do with any Flutter Project.
+
+![Flutter](https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white)
+
+![Dart](https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white)
+
+## License
+
+Distributed under the GPL License. See `LICENSE.txt` for more information.
+
+
+
+## Contact
+
+Pablo Olivares - [pablolivares1502@gmail.com](mailto:pablolivares1502@gmail.com)
+
+Project Link: https://github.com/pab1s/flutter-netflix-responsive-ui
+
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/cubits/cubits.dart';
+import 'package:flutter_netflix_responsive_ui/data/data.dart';
+import 'package:flutter_netflix_responsive_ui/widgets/widgets.dart';
+
+import '../cubits/cubits.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+    ..addListener(() {
+      context.read<AppBarCubit>().setOffset(_scrollController!.offset);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print('Cast'),
+        child: const Icon(Icons.cast),
+        backgroundColor: Colors.grey[850],
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50),
+        child: BlocBuilder<AppBarCubit, double>(
+          builder: (context, scrollOffset) {
+            return CustomAppBar(scrollOffset: scrollOffset);
+          }
+        ),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverToBoxAdapter(
+            child: ContentHeader(featuredContent: sintelContent),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 20),
+            sliver: SliverToBoxAdapter(
+              child: Previews(
+                key: const PageStorageKey('previews'),
+                title: 'Previews',
+                contentList: previews,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: const PageStorageKey('myList'),
+              title: 'My List',
+              contentList: myList,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: const PageStorageKey('originals'),
+              title: 'Netflix Originals',
+              contentList: originals,
+              isOriginals: true,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: const PageStorageKey('trending'),
+              title: 'Trending',
+              contentList: trending,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
